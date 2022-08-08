af71afc4847a011eb240de9a5b8977c5a44992aa
yuliaMasliak <1985kozak@gmail.com>
2022-08-08 23:51:19 +0300

    Actual date added

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = date.getDay();
+  let weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = weekday[date.getDay()];
+  return `${currentDay}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -11,6 +36,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#current-day");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "a2862d894f40dfa965c78c830dcfe9c5";
 let cityName = "Paris";
