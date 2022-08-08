8a39d65e38a7a87fa66883a6b4a023d0cbd0a4b2
yuliaMasliak <1985kozak@gmail.com>
2022-08-08 22:58:03 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "a2862d894f40dfa965c78c830dcfe9c5";
+let cityName = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
